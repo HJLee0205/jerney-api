dbdf51ed916a85d9c7a81e2c50198440d6782bf5
HuijaeLee <Huijae.Lee@cndfactory.com>
2023-03-10 15:28:02 +0900

    로그백 설정

--- src/main/java/com/hjlee/jerney/JerneyApplication.java
+++ src/main/java/com/hjlee/jerney/JerneyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class JerneyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(JerneyApplication.class, args);
 	}
 
 }
